7a4e414a1ef12b48f9c8b95052805ee59cbfacaf
Ocawesome101 <ocawesome101@gmail.com>
2020-06-22 19:22:56 -0400

    actually fix packaging

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ rm -rf dest pkg && mkdir -p dest pkg
 make -C src
 for file in $(ls dest); do
   echo $file
-  find dest/$file | cpio -o > $file.cpio
+  find dest/$file | cpio -o > packages/$file.cpio
 done
