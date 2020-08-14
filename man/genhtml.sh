04668082ab2c61cbf5183fdb98ff5cc2fcfc1aac
Ocawesome101 <ocawesome101@gmail.com>
2020-08-14 12:30:49 -0400

    fix some things

--- man/genhtml.sh
+++ man/genhtml.sh
@@ -7,10 +7,11 @@ mkdir -p ./web/man
 DOMAIN='oz-craft.pickardayune.com'
 
 echo '
+<link rel="stylesheet" href="https://oz-craft.pickardayune.com/blog/style.css">
 <html><title>Monolith manual pages</title>
 <body>
 <p>
-The following links point to all currently available manual pages from the Monolith system.
+The following links point to all currently available manual pages from the Monolith system.<br><br>If you notice any deficiencies, please open an issue on the <a href="https://github.com/ocawesome101/oc-monolith/issues">Monolith GitHub Issues page</a>.
 </p>
 ' > ./web/man/index.html
 
